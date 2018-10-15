c81c41d5a36a99fc1717b91e8fbe27a229f19edb
Benoit Penelle <benoit@penelle.net>
2018-10-15 09:13:58 +0200

    V07 - Ajout de Bootstrap et mise en place menu et navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MemberService } from './member.service';
 import { MemberListComponent } from './memberlist.component';
+import { LoginComponent } from './login.component';
+import { HomeComponent } from './home.component';
+import { UnknownComponent } from './unknown.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MemberListComponent
+        MemberListComponent,
+        LoginComponent,
+        HomeComponent,
+        UnknownComponent
     ],
     imports: [
         HttpClientModule,
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'members', component: MemberListComponent },
+            { path: '**', component: UnknownComponent }
+        ])
     ],
     providers: [
         MemberService
